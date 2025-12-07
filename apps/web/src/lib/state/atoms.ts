05ef54e8e601cbfe85deb9366e5624fdd1170ed6
Suyash Goel <suyashgoel10@gmail.com>
2025-12-06 23:05:26 -0800

    feat: add Jotai atom to persist original search query

--- apps/web/src/lib/state/atoms.ts
+++ apps/web/src/lib/state/atoms.ts
@@ -1,5 +1,4 @@
-import { UserResponse } from '@personal-website/shared';
 import { atom } from 'jotai';
 
-export const userAtom = atom<UserResponse | null>(null);
-export const searchQueryAtom = atom<string>('');
+// Search & Navigation
+export const searchQueryAtom = atom<string | null>(null);
