2683ce36897d97dece48687d93f5cccc56e61615
Suyash Goel <suyashgoel10@gmail.com>
2026-01-02 13:39:09 -0800

    fix: mark unused variable with underscore

--- apps/api/src/services/auth.ts
+++ apps/api/src/services/auth.ts
@@ -70,7 +70,7 @@ export async function login(user: LoginRequest): Promise<UserResponse> {
     throw new InvalidCredentialsError();
   }
 
-  const { hashedPassword, ...userResponse } = existingUser;
+  const { hashedPassword: _, ...userResponse } = existingUser;
 
   return userResponse;
 }
